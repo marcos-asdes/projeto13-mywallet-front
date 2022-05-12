27d1eb7779e8b63c5311c1c9ec0d41bbb87310bc
marcos.asdes <marco.engc@poli.ufrj.br>
2022-05-12 03:02:44 -0300

    added dependencies, index.js, App.js and UserContext.js

---
+++ src/components/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from '../contexts/UserContext.js';
+
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Main from './Main';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none !important;
+        font-family: 'Lexend Deca', sans-serif;
+        /* letter-spacing: 1px; */
+        font-weight: 300;
+        cursor: default;
+        -ms-overflow-style: none; /* for IE, Edge */
+        scrollbar-width: none; /* for Firefox */
+        transition: all 200ms ease-in-out 0s;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+        appearance: none;
+    }
+    *.hidden {
+        display: none !important;
+    }
+    *.disabled {
+        opacity: 0.4;
+        pointer-events: none;
+    }
+    div.root {
+        height: 100vh;
+        width: 100vw;
+        overflow-x: hidden;
+    }
+`
+export default function App() {
+
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+    const APILink = "http://localhost:5000/";
+
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <UserContext.Provider value={{APILink, user, setUser, token, setToken}}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/mywallet" element={<Main />} />
+                    </Routes>
+                </BrowserRouter>
+            </UserContext.Provider>
+        </React.Fragment>
+    )
+}
